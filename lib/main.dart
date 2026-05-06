4198c204409a1f0de31e1d2afcfd2ad79b9dbaca
TalhaSheikhNextpak <talha_sheikh@nextpak.org>
2026-05-06 15:06:55 +0500

    ios build

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'LimoGuy Driver',
+      title: 'Mayfair Driver',
       debugShowCheckedModeBanner: false,
       
       // Theme configuration
